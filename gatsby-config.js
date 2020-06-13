5e7bf00dcb8c1de65a48ae5d56a8553e5a6580b1
Ubuntu <ubuntu@ip-172-31-43-98.us-west-2.compute.internal>
2020-06-13 13:54:07 +0000

    change wordpress url for graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         fieldName: `wpgraphql`,
         // fieldName: `WordPress`,
         // Url to query from
-        url: `http://devbwp.enerbank.com/dev/graphql`,
+        url: `http://devgb.enerbank.com/graphql`,
 	      //url: `${config.wordPressUrl}/ennerbank/graphql`,
         refetchInterval: 60
       },
