6022b8bcdb62a7878d9b9dab94c42281311bd9f0
fancykingstar <fancykingstar2019@gmail.com>
2020-10-09 13:24:39 +0530

    css changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './header'
 import Footer from './footer'
 import '../css/layout.css'
+import '/https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'
 
 const Layout = ({ children }) => {	
 	return (
