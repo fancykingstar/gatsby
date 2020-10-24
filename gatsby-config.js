2afaa0befe31d4c6268b7e59a00b9939ec2acb0f
Gopal kumar <dev@lambentcreations.com>
2020-10-24 18:35:42 +0530

    add dedicated relationship service

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,52 @@
-
 module.exports = {
-  siteMetadata: {
-    title: `EnerBankUSA`,
-    description: `America's home improvement lender of choice`,
-    author: `@enerbank`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+      title: `EnerBankUSA`,
+      description: `America's home improvement lender of choice`,
+      author: `@enerbank`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: `WPGraphQL`,
-        // typeName: `WordPress`,
-        fieldName: `wpgraphql`,
-        // fieldName: `WordPress`,
-        url: `https://devgbpress.enerbank.com/graphql`,
-        refetchInterval: 60,
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-s3`,
-      options: {
-        bucketName: "my-website-bucket",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-nginx-redirect",
-      options: {
-        inputConfigFile: `${__dirname}/nginx.conf`,
-        outputConfigFile: `${__dirname}/nginx.out.conf`,
-        whereToIncludeRedirects: "server.http" // defaults to: "server"
-      },
-    },
-  ],
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: "gatsby-source-graphql",
+            options: {
+                typeName: `WPGraphQL`,
+                // typeName: `WordPress`,
+                fieldName: `wpgraphql`,
+                // fieldName: `WordPress`,
+                url: `https://devgbpress.enerbank.com/graphql`,
+                refetchInterval: 60,
+                headers: {
+                    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+                },
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                // background_color: `#663399`,
+                // theme_color: `#663399`,
+                // display: `minimal-ui`,
+                icon: `src/images/favicon-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-plugin-s3`,
+            options: {
+                bucketName: "my-website-bucket",
+            },
+        },
+    ],
 }
--- src/components/training.js
+++ src/components/training.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
+import close_icon from '../images/closed_icon.svg'
 
 const Training = (props) => {
 	if (props.popData.length !== 0) {
@@ -7,7 +8,7 @@ const Training = (props) => {
 			<Modal show={props.visiblity} animation={true} centered={true} size="full" id="appPopup">
 				<div className="heading_bg">
 					<button type="button" className="close text-white" data-dismiss="modal" aria-label="Close" onClick={props.handleClose}>
-						<img src="http://devgb.enerbank.com/wp-content/themes/enerbank/img/closed_icon.svg" width="25" alt="close icon" />
+						<img src={close_icon} width="25" alt="close icon" />
 					</button>
 					<h3 className="h2 font-weight-bold">Training</h3>
 				</div>
