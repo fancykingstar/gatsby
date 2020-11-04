fc41853d862efedbc012e9046e54591874aabe9c
Gopal kumar <dev@lambentcreations.com>
2020-11-04 23:23:36 +0530

    build after add feedback page

--- static/script.js
+++ static/script.js
@@ -1,2 +1,50 @@
-var hirebridge_client = '7798';var hirebridge_conf = {"filters":["search","location","group","sort"],"valuesFilter":[],"isOptionsFilter":0,"optionsFilter":[],"description":"400","top":"0","itempage":"10","showtitle":false,"groupBy":0,"groupByDisabled":true,"sortBy":1,"sortByDisabled":true,"font":"","h3font":"","fontsize":"","btnfont":"","btncolor":"FBB03B","btncolorhover":"FFFFFF","btnfontcolor":"FFFFFF","btnfontcolorhover":"000000","btnsize":"default","linkcolor":"000000","linkcolorhover":"FBB03B","formbackgroundcolor":"FFFFFF"};var hirebridge_lang = {'details': 'View Job Details &amp; Apply', 'new': 'New !', 'apply': 'Apply Now', 'all': 'Return To Job List', 'email': 'Share Job Link', 'title': 'jobs list', 'id': 'Job#'};(function() {var hirebridge = document.createElement('script');hirebridge.type = 'text/javascript';hirebridge.async = true;hirebridge.src = 'https://' + 'hirebridge.com/assets/portal10/js/hirebridge.js?v=1.16';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(hirebridge, s);})();(function() {var hirebridge_css = document.createElement('link');hirebridge_css.type = 'text/css';hirebridge_css.href = 'https://hirebridge.com/assets/portal10/css/hirebridge.css';hirebridge_css.rel = 'stylesheet';document.head.appendChild(hirebridge_css);})();
+var hirebridge_client = '7798';
+var hirebridge_conf = {
+    "filters":["search","location","group","sort"],
+    "valuesFilter":[],
+    "isOptionsFilter":0,
+    "optionsFilter":[],
+    "description":"400",
+    "top":"0",
+    "itempage":"10",
+    "showtitle":false,
+    "groupBy":0,
+    "groupByDisabled":true,
+    "sortBy":1,
+    "sortByDisabled":true,
+    "font":"",
+    "h3font":"",
+    "fontsize":"",
+    "btnfont":"",
+    "btncolor":"FBB03B",
+    "btncolorhover":"FFFFFF",
+    "btnfontcolor":"FFFFFF",
+    "btnfontcolorhover":"000000",
+    "btnsize":"default",
+    "linkcolor":"000000",
+    "linkcolorhover":"FBB03B",
+    "formbackgroundcolor":"FFFFFF"
+};
+var hirebridge_lang = {
+    'details': 'View Job Details &amp; Apply', 
+    'new': 'New !', 
+    'apply': 'Apply Now', 
+    'all': 'Return To Job List', 
+    'email': 'Share Job Link', 
+    'title': 'jobs list', 
+    'id': 'Job#'
+};
+(function() {
+    var hirebridge = document.createElement('script');
+    hirebridge.type = 'text/javascript';
+    hirebridge.async = true;
+    hirebridge.src = 'https://' + 'hirebridge.com/assets/portal10/js/hirebridge.js?v=1.16';
+    var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(hirebridge, s);
+})();
+(function() {
+    var hirebridge_css = document.createElement('link');
+    hirebridge_css.type = 'text/css';
+    hirebridge_css.href = '/hirebridge.css';
+    hirebridge_css.rel = 'stylesheet';document.head.appendChild(hirebridge_css);
+})();
 
